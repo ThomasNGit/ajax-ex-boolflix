b9296e61c0826e26ece915497f65a04bb2eab3b5
ThomasNGit <neronithomas1989@gmail.com>
2019-11-25 12:59:28 +0100

    Fixed the ajax call, added a forEach cicle and used it to find the desired films and added the results in page

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -6,13 +6,15 @@ cerca.click(ajaxCall);
 
 function ajaxCall(){
 
+    console.clear() //pulisco la console a ogni click
+
     var userInput = $(".user-input").val(); //variabile che determina il valore dell'input dell'utente
 
     console.log(userInput);
     
         $.ajax({ //creo la chiamata Ajax
 
-            url : "https://api.themoviedb.org/3/movie/550?api_key=9a6350f5129ed4894bf272b898b287df&query=" + userInput,
+            url : "https://api.themoviedb.org/3/search/movie?api_key=9a6350f5129ed4894bf272b898b287df&query=" + userInput,
 
             method : "GET",
 
@@ -20,27 +22,35 @@ function ajaxCall(){
 
                 console.log(data);
 
-                var films = "I film nel database sono: " + data.title //variabile che mi indica l'elenco dei film
+                var films = data.results //variabile che mi indica l'elenco dei film
 
-                console.log(films);
+                console.log("I film nel database sono: " + films);
 
-                var filmsItem;
+                var filmItem;
 
-                console.log("I film sono:");
+                var film = $(".result");
 
-                films.forEach(filmsItem =>{ //ciclo forEach per estrarre gli items dall'array
+                film.append('<div>Il film che hai cercato è : ' + userInput + '. Ecco i risultati della tua ricerca:</div>' + "<br>")
 
-                    console.log(filmsItem);
+                films.forEach(filmItem =>{
+                    console.log("Titolo: " + filmItem.title);
+                    film.append('<div>Titolo: ' + filmItem.title + '</div>');
+                    console.log("Titolo originale: " + filmItem.original_title);
+                    film.append('<div>Titolo originale: ' + filmItem.original_title + '</div>');
+                    console.log("Lingua originale: " + filmItem.original_language);
+                    film.append('<div>Lingua originale: ' + filmItem.original_language + '</div>');
+                    console.log("Valutazione: " + filmItem.vote_average);
+                    film.append('<div>Valutazione: ' + filmItem.vote_average + '</div>');
+                    console.log("---------");
+                    film.append('<div>-------------------</div>');
                     
+                })
                 
-                    
-
-                });
-                
+                $(".user-input").val(""); //resetto l'input field
             },
-            error : function(){
+            error : function(richiesta, stato, errori){
 
-                alert("error");
+                alert("E' avvenuto un errore. " + " " + richiesta + " " + stato + " " + errori);
 
             }
         })
