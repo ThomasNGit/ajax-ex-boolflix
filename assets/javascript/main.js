4db831f1b1ec138de53b4808bd1c1a5222d08dca
ThomasNGit <neronithomas1989@gmail.com>
2019-11-25 12:16:07 +0100

    create an ajax function

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,39 +1,50 @@
 $( document ).ready(function(){
 
-$(".user-search").click(function(){ //funzione che al click esegue una ricerca
+var cerca = $(".user-search");
+
+cerca.click(ajaxCall);
+
+function ajaxCall(){
 
     var userInput = $(".user-input").val(); //variabile che determina il valore dell'input dell'utente
 
-    console.log(userInput); //debug
-
-
-
-    $.ajax({
-
-        url : "https://api.themoviedb.org/3/movie/550?api_key=4592ff2ae6081cad0b67fb362589cc1a",
-
-        method : "GET",
-
-        success : function(data) {
-
-            var itemResponse = data.title;
-
-            console.log(itemResponse);
-
-            
-        },
-        error : function(){
-
-            alert("errore")
-
-        }
-
-    })
-
-    $(".user-input").val("")//ripulisco l'inputfield
+    console.log(userInput);
     
+        $.ajax({ //creo la chiamata Ajax
 
+            url : "https://api.themoviedb.org/3/movie/550?api_key=9a6350f5129ed4894bf272b898b287df&query=" + userInput,
 
-})
+            method : "GET",
+
+            success : function(data){
+
+                console.log(data);
+
+                var films = "I film nel database sono: " + data.title //variabile che mi indica l'elenco dei film
+
+                console.log(films);
+
+                var filmsItem;
+
+                console.log("I film sono:");
+
+                films.forEach(filmsItem =>{ //ciclo forEach per estrarre gli items dall'array
+
+                    console.log(filmsItem);
+                    
+                
+                    
+
+                });
+                
+            },
+            error : function(){
+
+                alert("error");
+
+            }
+        })
+}
+
 
 })
