39ea13f3a25f2b92f3fdaaa8cf7d65836d2671e8
ThomasNGit <neronithomas1989@gmail.com>
2019-11-25 10:41:25 +0100

    added click function and ajax call

---
+++ assets/javascript/main.js
@@ -0,0 +1,35 @@
+$( document ).ready(function(){
+
+$(".user-search").click(function(){ //funzione che al click esegue una ricerca
+
+    var userInput = $(".user-input").val(); //variabile che determina il valore dell'input dell'utente
+
+    console.log(userInput); //debug
+
+    $.ajax({
+
+        url : "https://api.themoviedb.org/3/movie/550?api_key=4592ff2ae6081cad0b67fb362589cc1a",
+
+        method : "GET",
+
+        success : function(data) {
+
+            console.log(data.response);          
+
+        },
+        error : function(){
+
+            alert("errore")
+
+        }
+
+    })
+
+    $(".user-input").val("")//ripulisco l'inputfield
+    
+})
+
+
+
+
+})
